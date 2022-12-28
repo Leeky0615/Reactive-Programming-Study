c03518f96bf301116fe12782fab07b3645c00a5f
Leeky0615 <rbdus7174@naver.com>
2022-12-28 13:48:49 +0900

    feat: study Cold Publisher & Hot Publisher

---
+++ src/main/java/practice/chatpor03/ColdPublisher.java
@@ -0,0 +1,11 @@
+package practice.chatpor03;
+
+import io.reactivex.rxjava3.core.Flowable;
+
+public class ColdPublisher {
+    public static void main(String[] args) {
+        Flowable<Integer> flowable = Flowable.just(1, 3, 5, 7);
+        flowable.subscribe(data -> System.out.println("Subscriber'A' : " + data));
+        flowable.subscribe(data -> System.out.println("Subscriber'B' : " + data));
+    }
+}
---
+++ src/main/java/practice/chatpor03/HotPublisher.java
@@ -0,0 +1,20 @@
+package practice.chatpor03;
+
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.processors.PublishProcessor;
+
+public class HotPublisher {
+    public static void main(String[] args) {
+        PublishProcessor<Integer> processor = PublishProcessor.create();
+        processor.subscribe(data -> System.out.println("Subscriber'A': " + data));
+        processor.onNext(1);
+        processor.onNext(3);
+
+        processor.subscribe(data -> System.out.println("Subscriber'B': " + data));
+        processor.onNext(5);
+        processor.onNext(7);
+
+        processor.onComplete();
+
+    }
+}
