a39ecaf575c834b8267c9daf832df3b800040a6c
Leeky0615 <rbdus7174@naver.com>
2023-01-02 18:24:05 +0900

    fix: change TimeUnit(MICROSECONDS -> MILLISECONDS)

--- src/main/java/practice/chapter05/operator/ObservableIntervalExample.java
+++ src/main/java/practice/chapter05/operator/ObservableIntervalExample.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 public class ObservableIntervalExample {
     public static void main(String[] args) {
         // 최초 통지 대기 시간 0L, 일정 대기 시간 1000L
-        Observable.interval(0L, 1000L, TimeUnit.MICROSECONDS)
+        Observable.interval(0L, 1000L, TimeUnit.MILLISECONDS)
                 .map(num -> num + " count")
                 .subscribe(data -> Logger.log(LogType.ON_NEXT, data));
 
--- src/main/java/practice/chapter05/operator/ObservableTimerExample.java
+++ src/main/java/practice/chapter05/operator/ObservableTimerExample.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 public class ObservableTimerExample {
     public static void main(String[] args) {
         Logger.log(LogType.PRINT, "# start");
-        Observable.timer(2000, TimeUnit.MICROSECONDS)
+        Observable.timer(2000, TimeUnit.MILLISECONDS)
                 .map(count -> "Do Work!")
                 .subscribe(data -> Logger.log(LogType.ON_NEXT, data));
 
